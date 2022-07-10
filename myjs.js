8b64cd9000ddd03b0fd0b52a9ab578edb4d988a8
Andreyleontyev001 <109039077+Andreyleontyev001@users.noreply.github.com>
2022-07-10 20:20:02 +0200

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,22 @@
+var op;
+function func() {
+var result;
+var num1 = Number(document.getElementById("num1").value);
+var num2 = Number(document.getElementById("num2").value);
+switch (op) {
+    case '+' :
+        result = num1 + num2
+        break;
+    case '-' :
+        result = num1 - num2
+        break;
+    case '*' :
+        result = num1 * num2
+        break;
+    case '/' :
+        result = num1 / num2
+        break;           
+}
+document.getElementById("result").innerHTML = result;
+
+}
